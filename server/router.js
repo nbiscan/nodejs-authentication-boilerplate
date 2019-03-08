2c638f1b7918ee163e68e36815deec521e65a8a6
nbiscan <nbiscan7@gmail.com>
2019-03-08 15:59:44 +0100

    Initialize project.

---
+++ server/router.js
@@ -0,0 +1,14 @@
+const Authentication = require("./controllers/authentication");
+const passportService = require("./services/passport");
+const passport = require("passport");
+
+const requireSignIn = passport.authenticate("local", { session: false });
+const requireAuth = passport.authenticate("jwt", { session: false });
+
+module.exports = function(app) {
+  app.get("/", requireAuth, function(req, res) {
+    res.send({ brki: "kralj" });
+  });
+  app.post("/signup", Authentication.signup);
+  app.post("/signin", requireSignIn, Authentication.signin);
+};
